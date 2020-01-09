0eb60a3d43d5d8c22c9f57ebf4fbbc00bada32d8
duanmo <duanmo@yonyou.com>
2020-01-09 14:14:35 +0800

    调用翻译接口

---
+++ src/main/java/com/flower/demo/service/tranlateService.java
@@ -0,0 +1,150 @@
+package com.flower.demo.service;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.nio.charset.StandardCharsets;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+
+public class tranlateService {
+//    private static Logger logger = LoggerFactory.getLogger(translateTest.class);
+
+    private static final String YOUDAO_URL = "https://openapi.youdao.com/api";
+
+    private static final String APP_KEY = "44ea58e18851119b";
+
+    private static final String APP_SECRET = "ODq6xfpMveysOcq8YpEKz9XCA8SoQXHL";
+
+    public String getChineseName(String englishName) throws IOException {
+
+        Map<String, String> params = new HashMap<>();
+        String q = "rose";
+        String salt = String.valueOf(System.currentTimeMillis());
+        params.put("from", "en");
+        params.put("to", "zh-CHS");
+        params.put("signType", "v3");
+        String curtime = String.valueOf(System.currentTimeMillis() / 1000);
+        params.put("curtime", curtime);
+        String signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET;
+        String sign = getDigest(signStr);
+        params.put("appKey", APP_KEY);
+        params.put("q", q);
+        params.put("salt", salt);
+        params.put("sign", sign);
+        /* 处理结果 */
+        return requestForHttp(params);
+    }
+
+    private String requestForHttp(Map<String, String> params) throws IOException {
+
+        /** 创建HttpClient */
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        /** httpPost */
+        HttpPost httpPost = new HttpPost(YOUDAO_URL);
+        List<NameValuePair> paramsList = new ArrayList<>();
+        Iterator<Map.Entry<String, String>> it = params.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> en = it.next();
+            String key = en.getKey();
+            String value = en.getValue();
+            paramsList.add(new BasicNameValuePair(key, value));
+        }
+        httpPost.setEntity(new UrlEncodedFormEntity(paramsList, "UTF-8"));
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+        try {
+            HttpEntity httpEntity = httpResponse.getEntity();
+            String json = EntityUtils.toString(httpEntity, "UTF-8");
+            EntityUtils.consume(httpEntity);
+            Map resultMap = JSON.parseObject(json);
+            String jsonObject = resultMap.get("web").toString();
+            ArrayList arrayList = JSON.parseObject(jsonObject,ArrayList.class);
+            Map<String,Object> objectMap = (Map<String, Object>) arrayList.get(0);
+            ArrayList nameList = JSON.parseObject(objectMap.get("value").toString(),ArrayList.class) ;
+
+            return nameList.get(0).toString();
+        } finally {
+            try {
+                if (httpResponse != null) {
+                    httpResponse.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 生成加密字段
+     */
+    public static String getDigest(String string) {
+        if (string == null) {
+            return null;
+        }
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        byte[] btInput = string.getBytes(StandardCharsets.UTF_8);
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance("SHA-256");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (byte byte0 : md) {
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param result 音频字节流
+     * @param file   存储路径
+     */
+    private static void byte2File(byte[] result, String file) {
+        File audioFile = new File(file);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(audioFile);
+            fos.write(result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private static String truncate(String q) {
+        if (q == null) {
+            return null;
+        }
+        int len = q.length();
+        String result;
+        return len <= 20 ? q : (q.substring(0, 10) + len + q.substring(len - 10, len));
+    }
+}
