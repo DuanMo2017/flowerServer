f45d61bb9bd3b527e7ff74dde1a840d913bc40cd
duanmo <duanmo@yonyou.com>
2020-01-15 08:48:03 +0800

    清除无用代码

--- src/main/java/com/flower/demo/service/tranlateService.java
+++ src/main/java/com/flower/demo/service/tranlateService.java
@@ -1,7 +1,6 @@
 package com.flower.demo.service;
 
 import com.alibaba.fastjson.JSON;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -11,8 +10,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import java.nio.charset.StandardCharsets;
 import java.io.*;
 import java.security.MessageDigest;
